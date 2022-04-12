1a66718fb3c859f8cc4ca29680a7e52e0a7343e2
ayushkp <96825046+Ayushkvp@users.noreply.github.com>
2022-04-13 01:35:49 +0530

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+// function play() {
+//     let navid = document.getElementById("navbar");
+//     let respnav = document.getElementById("respnav");
+//     if (navid.className ==="navbar"){
+//         navid.className += "navbarresp";
+//         // respnav.className += "navlist";
+//         // console.log("hello")
+//     } else{
+//         navid.className = "navbar";
+//         // respnav.className = "navlist";
+//     }
+// }
